bbe07cbbf001b60124882803c505d049453b8614
Gabriel Rodrigues <gabrielws31@gmail.com>
2020-02-24 14:38:11 -0300

    faltando apenas enviar formulario de contato e footer

--- public_html/wp-content/themes/bt-sass-blank-theme/inc/constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-// I18n theme domain
-define('I18N_DOMAIN', 'bsbt');
-
-// Acf installed ?
-//define('ACF_EXISTS', (bool)function_exists('get_field') ); //@TODO : uncomment if you're using ACF
---
+++ wp-content/themes/bt-sass-blank-theme/page-contact.php
@@ -0,0 +1,12 @@
+<?php
+	/* Template Name: Contato */
+	get_header();
+?>
+<section class="contact-section">
+ <?php
+    get_template_part('template-parts/page/content', 'contact');
+ ?>
+</section>
+<?php
+	get_footer();
+?>
